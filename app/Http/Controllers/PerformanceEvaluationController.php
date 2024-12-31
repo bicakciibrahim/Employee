f7690deec09db6a91d4b70e87d77a4b137f5ef26
İbrahim BIÇAKCI <ibrahim_bicakci29@hotmail.com>
2024-12-31 14:52:28 +0300

    rota düzenlemeleri

--- app/Http/Controllers/PerformanceEvaluationController.php
+++ app/Http/Controllers/PerformanceEvaluationController.php
@@ -27,17 +27,17 @@ class PerformanceEvaluationController extends Controller
         return view('performance_evaluations.create', compact('employees'));
     }
 
-
+    // Performans Değerlendirmesi Kaydetme
     public function store(Request $request)
     {
-
+        // Gelen veriyi validate et
         $request->validate([
             'calisan_id' => 'required|exists:calisanlar,id',
             'puan' => 'required|integer|min:1|max:100',
             'yorum' => 'required|string',
         ]);
 
-
+        // Performans Değerlendirmesini kaydet
         PerformanceEvaluation::create([
             'calisan_id' => $request->calisan_id,
             'puan' => $request->puan,
